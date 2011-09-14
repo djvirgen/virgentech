37d3f9e979bce920d1185e080fc2dab3374d2bca
Hector Virgen <hector.virgen@disney.com>
2011-09-14 09:00:56 -0700

    Initial commit of empty rails project for new VirgenTech.com

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Virgentech::Application.config.secret_token = 'd4be2f53b492c4d18d3c3127f34b3c8a16f890aa6d05e812fe3c279d43b9278dadfd2b184bf711dd7aa9fcabe865bf6c5763a8d963ad9b0d164f4271a0ecb14d'
