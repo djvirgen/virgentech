8c97ef96c6bc6940bacbc8455d8dd7fd5addd665
Hector Virgen <djvirgen@gmail.com>
2011-09-20 22:54:19 -0700

    Adds validations to User model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110921053840) do
+ActiveRecord::Schema.define(:version => 20110921055316) do
 
   create_table "blogs", :force => true do |t|
     t.string   "url"
@@ -29,5 +29,7 @@ ActiveRecord::Schema.define(:version => 20110921053840) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
