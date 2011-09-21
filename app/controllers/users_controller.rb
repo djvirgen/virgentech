859190d590f3aec5729d161976f1e1182b2af179
Hector Virgen <djvirgen@gmail.com>
2011-09-20 23:50:08 -0700

    Adds signup form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,21 @@ class UsersController < ApplicationController
 	end
 
   def new
+	  @user = User.new
+
+		respond_to do |format|
+			format.html
+			format.xml  { render :xml => @user }
+		end
   end
 
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			flash[:success] = "Welcome, #{@user.name}"
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
 end
