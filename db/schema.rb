5a329ac5ba03ef8cd03591334499ee8184e86ff5
Hector Virgen <djvirgen@gmail.com>
2011-09-20 23:21:02 -0700

    Adds basic user authentication.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110921055316) do
+ActiveRecord::Schema.define(:version => 20110921061308) do
 
   create_table "blogs", :force => true do |t|
     t.string   "url"
@@ -25,9 +25,10 @@ ActiveRecord::Schema.define(:version => 20110921055316) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
+    t.string   "encrypted_password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
