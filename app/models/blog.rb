6f638a18a98d03a199ebf6bc425956087f644738
Hector Virgen <djvirgen@gmail.com>
2011-09-20 21:33:41 -0700

    Adds published scope to blog model.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,22 +1,14 @@
 class Blog < ActiveRecord::Base
+	# Pagination
 	self.per_page = 10
+	
+	# Scopes
+	scope :published, where('published < ?', DateTime.now)
 	
 	# Finds a single post based on published year/month and url
   def self.find_published(year, month, url)
-    now = DateTime.now
-
     date_start = DateTime.new(year.to_i, month.to_i, 1)
-    # Do not return blog entries from the future
-    if (date_start > now)
-      return nil
-    end
-
     date_end = date_start >> 1
-    # Again, ensure that future blog entries are not returned
-    if (date_end > now)
-      date_end = now
-    end
-
     self.where(:url => url, :published => date_start..date_end).first
   end
 
